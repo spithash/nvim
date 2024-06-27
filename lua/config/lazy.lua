afeb8b5a160da74f56417f0a7fe19de4ec45e177
spithash <spithash@tuta.io>
2024-06-27 11:34:21 +0300

    removed dap core

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -42,7 +42,6 @@ require("lazy").setup({
     },
 
     -- import any extras modules here
-    { import = "lazyvim.plugins.extras.dap.core" },
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
     -- { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
